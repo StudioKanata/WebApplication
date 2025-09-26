502bfcadac94aea04b8de81a85e4f4faac3b3b3d
StudioKanata <kanata.studio0138@gmail.com>
2025-09-26 20:50:51 +0900

    クライアント側からタスクを追加するできるようにする

--- src/main.go
+++ src/main.go
@@ -14,6 +14,13 @@ func handleTodo(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, todoList)
 }
 
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	todo := r.Form.Get("todo")
+	todoList = append(todoList, todo)
+	handleTodo(w, r)
+}
+
 func main() {
 	todoList = append(todoList, "顔を洗う", "朝食を食べる", "歯を磨く")
 
@@ -21,26 +28,12 @@ func main() {
 	// http.FileServerに渡すパスを調整staticを除いてパスを検索
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", handleTodo)
+	http.HandleFunc("/todo", handleTodo)
+
+	http.HandleFunc("/add", handleAdd)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("failed to start server : ", err)
 	}
-
-	/**
-	  func hello(w http.ResponseWriter, req *http.Request) {
-	  	fmt.Fprint(w, "Hello, Web application!")
-	  }
-	*/
-
-	//func main() {
-	//	// リクエストに対する処理の登録
-	//	// 指定したdirectoryのファイルを返す
-	//	http.Handle("/", http.FileServer(http.Dir("static/")))
-	//	// HTTPサーバーを起動する関数で、ポート8080で受け付ける
-	//	err := http.ListenAndServe(":8080", nil)
-	//	if err != nil {
-	//		log.Fatal("failed to start : ", err)
-	//	}
 }
