3bea36bf2f9f7fdf4c0b5623ffa53b072635ed32
StudioKanata <kanata.studio0138@gmail.com>
2025-09-26 23:19:51 +0900

    PRGパターンでPOSTの繰り返しを修正

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 
 var todoList []string
 
+const REDIRECT int = 303
+
 func handleTodo(w http.ResponseWriter, r *http.Request) {
 	// 本来なら、戻り値にerrを受け取りハンドリングする必要がある
 	t, _ := template.ParseFiles("templates/todo.html")
@@ -18,7 +20,9 @@ func handleAdd(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	todo := r.Form.Get("todo")
 	todoList = append(todoList, todo)
-	handleTodo(w, r)
+	//handleTodo(w, r)
+	// /todoにリダイレクト
+	http.Redirect(w, r, "/todo", REDIRECT)
 }
 
 func main() {
@@ -27,11 +31,8 @@ func main() {
 	// HTML内から読み込むCSSファイルを返すため
 	// http.FileServerに渡すパスを調整staticを除いてパスを検索
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
 	http.HandleFunc("/todo", handleTodo)
-
 	http.HandleFunc("/add", handleAdd)
-
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("failed to start server : ", err)
